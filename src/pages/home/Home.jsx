eb3488bc5e58e7344c5551bf4110dbb02583ff1a
rishi-patel1 <patelrishiofficial@gmail.com>
2022-08-27 00:26:50 +0530

    added values component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import MainHeader from '../../components/MainHeader'
 import Programs from '../../components/Programs'
+import Values from '../../components/Values'
 import './home.css'
 
 
@@ -8,6 +9,7 @@ const Home = () => {
     <>
     <MainHeader/>
     <Programs/>
+    <Values/>
     </>
   )
 }
