16aacd6a272f4cff97c59df9f898c3d508bfb44e
rishi-patel1 <patelrishiofficial@gmail.com>
2022-09-01 01:06:56 +0530

    worked on contact component

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,27 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_2.jpg'
+import {MdEmail} from 'react-icons/md'
+import {BsMessenger} from 'react-icons/bs'
+import {IoLogoWhatsapp} from 'react-icons/io'
+
 import './contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <Header title="Get In Touch" image ={HeaderImage}>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa odio debitis facilis quod illo rem.
+    </Header>
+    <section className="contact">
+      <div className="container contact__container">
+        <div className="contact__wrapper">
+          <a href="mailto:cpointonline@gmail.com" target="_blank" rel = "noreferrer noopener"><MdEmail/></a>
+          <a href="https://m.me/cpointonline" target="_blank" rel = "noreferrer noopener"><BsMessenger/></a>
+          <a href="https://wa.me/+919879516709" target="_blank" rel = "noreferrer noopener"><IoLogoWhatsapp/></a>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
